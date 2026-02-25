d9bc72f380390a1badeb06c7735066a38dc84efc
fabricekabeyayanga-eng <fabricekabeyayanga@gmail.com>
2026-02-25 14:00:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "faky-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/faky/",
+        "/faky/index.html"
+      ]);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
